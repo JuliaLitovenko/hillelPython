cdfb7ac1ed78f0935620810850767963c32540e9
JuliaLitovenko <julia.litovenko@gmail.com>
2022-07-15 08:20:23 +0300

    Add files via upload

---
+++ assignment01_01 (1).py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Assignment 01-01
+#
+# Assign actual data to the variables below
+# and run the script to print the output.
+
+name = "Yuliia"
+profession = "QA Manual"
+facility = "AbSoft"
+city = "Odessa"
+country = "Ukraine"
+goal = "AQA Engineer"
+age = 28
+
+print(
+f"""
+My name is {name}. I am a {profession} at {facility} 
+in {city}, {country}. I want to learn how to program
+in Python to become a {goal}. I am {age} years old.
+"""
+) 
