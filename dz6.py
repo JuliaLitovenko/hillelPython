5a535da8cba7a71f8d5c37d69e47e22299642ced
JuliaLitovenko <julia.litovenko@gmail.com>
2022-07-31 21:38:22 +0300

    Update dz6.py

--- dz6.py
+++ dz6.py
@@ -1,23 +1,21 @@
 
 
 
-str = input('Введіть текст:')
+text = input('Введіть текст:')
 user_letter = input('Введіть літеру:')
 y = len(user_letter)
-count = str.count(user_letter)
+count = text.count(user_letter)
 
 if len(user_letter) > 1:
    print('Введіть лише одну букву')
-else:
-    pass
 
 
 
-if user_letter in str and count <= 10:
+if  count > 0 and count < 10:
     print(f'Строка містить літеру "{user_letter}" до 10 разів' )
-elif user_letter in str and (count >=10) and  (count <= 20):
+elif count >=10 and  count <= 20:
     print (f'Строка містить літеру "{user_letter}" до 20 разів' )
-elif user_letter in str and count >= 20 :
+elif  count >= 20 :
    print(f'Строка містить літеру "{user_letter}" більше 20 разів')
 else:
     print ("Такої літери в тексті не виявлено")
