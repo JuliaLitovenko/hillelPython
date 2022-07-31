e2e0576dacb5fe73879baa35f1e6a271b4f575d1
JuliaLitovenko <julia.litovenko@gmail.com>
2022-07-31 21:30:42 +0300

    Update dz7.py

--- dz7.py
+++ dz7.py
@@ -6,7 +6,7 @@ data = {}
 # генеруємо рандомне число від 1 до 100, а потім створюємо послідовність від 0 до сгенерованого числа
 for i in range(random.randint(1, 100)):
     data[i] = f"item_{i}"  # наповнюємо словник відповідно до послідовності рандомної довжини
-if search_key == data[i]:
+if search_key in data.keys():
     print(f"Елемент {search_key}: {data[search_key]}")
 else:
     print (f'Помилка: елемент з ключем {search_key} відсутній')
